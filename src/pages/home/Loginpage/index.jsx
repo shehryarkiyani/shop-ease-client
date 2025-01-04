0ef2ca27c52824a1443127f43837af61d0c811a6
Zubair-mir0 <zubairmir05@gmail.com>
2025-01-04 18:10:57 +0500

    added images login page fixed

--- src/pages/home/Loginpage/index.jsx
+++ src/pages/home/Loginpage/index.jsx
@@ -66,32 +66,18 @@ const Loginpage = () => {
       </div>
 
       <div className="right "> 
-      <div className="relative h-screen w-full">
+      <div className="relative  w-full">
  
   <img
-    className="h-screen w-full object-cover"
-    src="https://s3-alpha-sig.figma.com/img/d333/e56b/7ee8146f4219496b153df11b78a5fbbd?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hNluKlbS3LlJcGbll~9xCMAw5VEKMBV5Gmg9Y-z-1kM86s8hGzMKRY4yAIEcbXl8kSY7ENbGJcdOTC6E7RJbIM5AAz6pwvFoJ7-Ampz9AapOZ4YFMRZvJ6uY6v9rulFvUyR8NGb~6y8lbrKRySC5z3H1SJ~0ExpzokOPQKeH07QrfLOuWG2hOpgX8Jgn8sbdzclDlaTrJpGV5mwz8XXpTvk5YjhOE4TJnqJzwkPg~E41ssvPG8Vqlyh~puxzTmi7d9gyUbqHG20y3Zys-cPQK38Bqh291VTQb8qAdjr5UEA6SDwxwTlqgUSFbFyomNMyzOuo4Doss5px6bZh-LJGuQ__"
+    className="h-screen w-full object-contain"
+    src="/assets/Group-680.png"
  
   />
+        
+        <div className="absolute inset-0 bg-black/20"></div>
 
-  
-  <div className="absolute inset-0 bg-black/50"></div>
 
-  <div className="absolute top-2 left-2 w-[170px] h-[170px]">
-    <img
-      className="w-full h-full"
-      src="https://s3-alpha-sig.figma.com/img/5b77/3210/66292fba05810bb6bfb320ccf8115e06?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VJElkngE-I7ruSKUXWfsw9N4~M9t0xRdKH3BuIcw0PHJk0NLE5iUmxUTVLaKO2Urt96-jsgFTCCNddrnTERYjsA1IkhktjFGU5A12fGhZz5ohe5XTjVeI-IwFviPRQcwb1UEwKZxM-7VywSBRl1w04KC2hVBtj2ag4BvpuJyCeP723NPbR0v3bqLzw3ZISoRrVIb-EBxL-7MdDH6XDlDAgvvjnEM8wpvc9JVlAnWDLqoCBJ7w8evylzWK9hf8fJuEM4DUwVYb0jfc~k~rPLjCuv~KaBSJQdufh6qVWVStk2kUvhj2qXoTugRCJxR1VCrYpxhI0hH-jbwwsQtLIYEYw__"
-     
-    />
-  </div>
 
-  <div className="absolute top-[55%] right-32 w-[485px]  transform -translate-y-1/2">
-    <img
-      className="w-full h-full rounded-2xl"
-      src="https://s3-alpha-sig.figma.com/img/470b/3c53/9ea4c754ffacefd83069e021c6ad0a28?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W92DiF7pRID8h1uuVXElU-A0-y4CWMrmElmviYRfSgfJ30ykAgWs~Bzz6AR7uI~dyZtpdGwUUb5RpcoUlDz6sZVaJAa81GIvY~2vEeosuea7J0borIJ5dNJUCEXeiAw6ancQaplOz2Lwll8ygMvtgr9JZ1JokHFqXHa07hWuJfKqi25gwxNVW4-2j~odNYaUz-WHRPMaqaS8X-6hmiKaONzftHK7h-al3YSCvk-HUTiCh4z23eVCl43WULcVVKgKG2hDUoGzXLU9944FlWEGd~RMi2agVv0UrmIx8QLabCSSvYb-ihgyxD-5A6kPiA877B2cylb97V2kRF1oK4QTlw__"
-    
-    />
-  </div>
 
   <div className="absolute top-[470px] right-36 text-center text-white">
     <h1 className="text-2xl font-bold">Buy the best items on our site.</h1>
