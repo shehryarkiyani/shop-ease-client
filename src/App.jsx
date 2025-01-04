504bda1bdca795241a13bc721737fece3591ea60
Zubair-mir0 <zubairmir05@gmail.com>
2025-01-04 17:41:01 +0500

    feat: basic login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import AppRouter from "core/router/router";
+// import AppRouter from "core/router/router";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import { RouterProvider } from "react-router-dom";
+// import { RouterProvider } from "react-router-dom";
+import Loginpage from "./pages/home/Loginpage";
+
 function App() {
   return (
     <>
-      <RouterProvider router={AppRouter()} />
+      {/* <RouterProvider router={AppRouter()} /> */}
+      <Loginpage/>
     </>
   );
 }
